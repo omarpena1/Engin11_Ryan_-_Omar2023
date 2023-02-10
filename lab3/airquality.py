a4cc0a2e7755e4a22641face6eab94ae85be0c45
Ryan Sun <sunryan20@gmail.com>
2023-02-10 15:41:40 -0800

    add lab3 data

---
+++ lab3/airquality.py
@@ -0,0 +1,63 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+
+
+reset_pin = None
+
+import serial
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+
+# Create library object, use 'slow' 100KHz frequency!
+#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+#pm25 = PM25_I2C(i2c, reset_pin)
+
+import time
+
+f = open("airpollutiondata.csv", "w")
+
+itime = time.time()
+
+metadata = ["time", "Concentration Units (Standard) PM 1.0","Concentration Units (Standard) PM 2.5","Concentration Units (Standard) PM 10.0"]
+
+for entry in metadata:
+    f.write(entry + ",")
+
+f.write("\n")
+
+print("Found PM2.5 sensor, reading data...")
+
+for _ in range(30):
+    time.sleep(1)
+    
+    try:
+        aqdata = pm25.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+    
+    numbersdata = []
+    numbersdata.append(itime)
+    
+    print(aqdata["pm10 standard"])
+    print(aqdata["pm25 standard"])
+    print(aqdata["pm100 standard"])
+    
+    numbersdata.append(aqdata["pm10 standard"])
+    numbersdata.append(aqdata["pm25 standard"])
+    numbersdata.append(aqdata["pm100 standard"])
+
+    
+    for idata in numbersdata:
+        f.write(str(idata)+",")
+
+    f.write("\n")
+
+f.close()
