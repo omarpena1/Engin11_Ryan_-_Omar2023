dbf96016b1a589a73e8a514a0ac3b2e0750e147f
Ryan Sun <sunryan20@gmail.com>
2023-03-17 15:00:24 -0700

    script

--- lab9/radiationscript.py
+++ lab9/radiationscript.py
@@ -2,6 +2,7 @@
  
 import RPi.GPIO as GPIO
 import datetime
+import time
  
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
@@ -9,14 +10,18 @@ def my_callback(channel):
     else:
         print('\n ▲ at ' + str(datetime.datetime.now())) 
  
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
  
-    message = input('\nPress any key to exit.\n')
- 
-finally:
-    GPIO.cleanup()
+for i in range(0,30):
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(6, GPIO.IN)
+        GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
+     
+        message = input('\nPress any key to exit.\n')
+     
+    finally:
+        GPIO.cleanup()
+        
+    time.sleep(1)
  
 print("Goodbye!")
