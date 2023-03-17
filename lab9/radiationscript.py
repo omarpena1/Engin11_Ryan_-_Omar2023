ffc1923782ca395abaae45de0d5c68db083c6e71
Ryan Sun <sunryan20@gmail.com>
2023-03-17 14:59:31 -0700

    add radiation script

--- lab9/radiationscript.py
+++ lab9/radiationscript.py
@@ -1,22 +1,5 @@
-# coding=utf-8
- 
 import RPi.GPIO as GPIO
 import datetime
- 
-def my_callback(channel):
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now())) 
- 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
- 
-    message = input('\nPress any key to exit.\n')
- 
-finally:
-    GPIO.cleanup()
- 
-print("Goodbye!")
+
+channel = GPIO.wait_for_edge(channel, GPIO_FALLING)
+print('Edge detected on channel', channel)
