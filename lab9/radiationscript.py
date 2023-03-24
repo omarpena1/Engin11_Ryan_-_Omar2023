f901d1827569d2b348572a576a4555a176c8a700
Ryan Sun <sunryan20@gmail.com>
2023-03-24 14:24:58 -0700

    new radiation script

--- lab9/radiationscript.py
+++ lab9/radiationscript.py
@@ -1,21 +1,38 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import sys
 
 GPIO.setmode(GPIO.BCM)    
 channel = 6
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+assert len(sys.argv) == 4, "use the correct number of arguments!"
 
-while True:
+file_name = sys.argv[3]
+        
+f = open(file_name, "w")
+
+f.write("timestamp,")
+
+f.write("counts per " + sys.argv[2] + " seconds")
+
+f.write("\n")
+
+startTime = time.time()
+
+while time.time() - startTime < int(sys.argv[1]):
     c = 0
-    startTime = time.time()
-    while (time.time() - startTime < 60):
-        c = c+1
+    loopTime = time.time()
+    while time.time() - loopTime < int(sys.argv[2]):
         GPIO.wait_for_edge(channel, GPIO.FALLING)
         print("Radiation Detected At", str(datetime.datetime.now()))
-    print("Counts Last Minute", c)
-    
+        c = c+1
+    print("Counts in the last " + sys.argv[2] + " seconds", c)
+    f.write(str(time.time()) + ",")
+    f.write(str(c))
+    f.write("\n")
+
     
     
     
